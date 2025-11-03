aa921fe602b4aaea3286d585c4bbf8f64d9d9e7f
Xhuljano Hamzallari <xhamzall@c2r3p4.42firenze.it>
2025-11-03 19:10:23 +0100

    update

---
+++ cpp01/module06/ex00/ScalarConverter.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xhamzall <xhamzall@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 16:24:59 by xhamzall          #+#    #+#             */
+/*   Updated: 2025/11/03 19:04:51 by xhamzall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+ScalarConvert::ScalarConvert(){}
+
+ScalarConvert::ScalarConvert(const ScalarConvert &obj)
+{
+	(void)obj;
+}
+
+ScalarConvert& ScalarConvert::operator=(const ScalarConvert &obj)
+{
+	(void)obj;
+	return *this;
+}
+
+ScalarConvert::~ScalarConvert(){}
+
+void ScalarConvert::converter(std::string )
+{
+
+}
