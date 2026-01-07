a7ba6935f4a596353cba34af3306ad049fd7d069
Xhuljano Hamzallari <xhamzall@c2r4p6.42firenze.it>
2026-01-07 15:58:43 +0100

    add Ex01

---
+++ cpp01/module09/ex01/RPN.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xhamzall <xhamzall@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/07 12:46:35 by xhamzall          #+#    #+#             */
+/*   Updated: 2026/01/07 14:50:40 by xhamzall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <fstream>
+#include <time.h>
+
+class BitcoinExchange
+{
+	private:
+		std::map <time_t, float> btc;
+		bool valid;
+	public:
+		
+};
+
+#endif
