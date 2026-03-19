60c7dce630af6858b210888f29a56a7ba193458f
xhamzall <xhuljanohamzallari42@gmail.com>
2026-03-19 19:33:08 +0100

    checks

--- Inception/src/requirements/Mariadb/Tools/mariadb.sh
+++ Inception/src/requirements/Mariadb/Tools/mariadb.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then
-	echo "The databese is already present"
+	echo "The database is already present"
 else
 	echo "The database is missing: creating..."
 
 	mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-	mysqld_safe &
+	mysqld_safe&
 	
 	sleep 5
 
-	mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+	mysql -e "CREATE DATABASE IF NOT EXISTS\`${MYSQL_DATABASE}\`;"
 	mysql -e "CREATE USER IF NOT EXISTS\`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 	mysql -e "GRANT ALL PRIVILEGES ON\`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
 	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOTPASSWORD}';"
